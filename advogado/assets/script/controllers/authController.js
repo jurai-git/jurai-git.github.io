54e79e7eb09318e892a0354dcdaf5f904fcf6ab7
wendelfrota <wendelalves898@gmail.com>
2024-11-10 18:13:58 -0300

    feat: add auth controller

---
+++ advogado/assets/script/controllers/authController.js
@@ -0,0 +1,37 @@
+import CookieService from '../../../../assets/js/services/cookieService.js';
+
+class AuthController {
+    static async init() {
+        try {
+            const accessToken = await CookieService.getCookie('access_token');
+
+            if (!accessToken.value) {
+                console.warn('Cookie not found');
+                window.location.href = '../login.html';
+            }
+            document.getElementById('logout')?.addEventListener('click', this.handleLogout)
+
+        } catch (error) {
+            window.location.href = '../login.html';
+            console.error('Error checking authentication. ', error);
+        }
+    }
+
+    static async handleLogout() {
+        const response = confirm('Deseja sair da conta?');
+
+        if (response == true) {
+            try {
+                await CookieService.removeCookie('access_token');
+                window.location.href = '../index.html';
+            } catch (error) {
+                const error_message = `Error when logging out: ${error}`
+
+                console.error(error_message);
+                alert('Erro ao sair da conta.\n', error_message)
+            }
+        }
+    }
+}
+
+AuthController.init();
